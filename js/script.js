91fde27385f2f3b8064b83579b45988080239000
AhmedReyad228 <151575265+AhmedReyad228@users.noreply.github.com>
2023-11-23 13:19:12 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 1000;
+// let started = false;
+
+// window.onscroll = function(){
+//     if (window.scrollY >= valueDisplays.offsetTop){
+//         if(!started){
+//             num.forEach((num) => startValue(num));
+//         }
+//         started = true;
+//     }
+// };
+
+
+
+valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute
+    ("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function() {
+        startValue += 1;
+        valueDisplay.textContent = startValue;
+        if(startValue == endValue){
+            clearInterval(counter);
+        }
+    }, duration);
+});
+
+
+
